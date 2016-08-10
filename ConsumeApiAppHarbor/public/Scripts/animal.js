36ac973d388e0ad9e20f5f7ddad4ac7b397d8f17
Jaime A. Esquivel <jaimeesquivelgomez@gmail.com>
2016-08-10 02:08:21 -0600

    no funciona el cross domain no lo logre

--- ConsumeApiAppHarbor/public/Scripts/animal.js
+++ ConsumeApiAppHarbor/public/Scripts/animal.js
@@ -15,16 +15,26 @@ $(document).ready(function () {
 });
 
 function AddNewAnimal(animal){
-var urlApi = 'http://quizprojectapi.apphb.com/api/Animal'
+var ulrApi = "http://quizprojectapi.apphb.com/api/Animal";
     $.ajax({
-        url: urlApi,
-        type:'POST',
-        contentType: "application/json;chartset=utf-8",
-        data: JSON.stringify(animal),
-        success:function () {
-            GetAll();
-            ClearForm();
-            alert("You save a new animal");
+        url: ulrApi,
+        type: "POST",
+        headers:{
+            'Access-Control-Allow-Origin': 'http://quizprojectapi.apphb.com',
+        },
+        data:JSON.stringify(animal),
+        crossDomian:true,
+        contentType: "application/json;charset=utf8",
+        statusCode: {
+            201: function () {
+                GetAll();
+                ClearForm();
+                alert("You create a new Person");
+            },
+            400: function () {
+                ClearForm();
+                alert("Error");
+            }
         }
     });
 }
@@ -36,12 +46,15 @@ function GetAll(){
     $.ajax({
         url: urlApi,
         type:'GET',
+        crossDomian:true,
+        dataType:'jsonp',
         success:function (data) {
             $.each(data,function (key,value) {
                 item +='<tr>'
                         +'<td>'+value.Nombre+'</td>'
                     +'<td>'+value.Tipo+'</td>'
                     +'<td>'+value.Descripcion+'</td>'
+                    +'<td>'+holis+'</td>'
                     +'</tr>';
             });
             $('#AnimalList').append(item);
@@ -49,6 +62,21 @@ function GetAll(){
     })
 }
 
+//Function de get a specific record
+function GetAnimalById(id) {
+    var url = "/api/Animal/" + id;
+    $.getJSON(url).done(function(Person) {
+        if (Person != null) {
+            $("#txtNombre").val(Animal.Nombre);
+            $("#txtTipo").val(Animal.Tipo);
+            $("#txtDescripcion").val(Animal.Descripcion);
+        } else {
+            alert("Animal not Exist");
+        }
+    }).fail(function(error) {
+        alert(error.toString());
+    });
+};
 function ClearForm(){
     $('#txtName').val("");
     $('#txtType').val("");
